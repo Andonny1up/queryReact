3ea3d2ef529becd24de0c824ecd31ceeaa68e974
Andoni1Barba <Andoni1barba@gmail.com>
2024-11-20 14:51:35 -0400

    react router init

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,7 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { useRef } from "react";
-import { Post } from "./types";
-import useCreatePost from "./hooks/useCreatePost";
-import useDeletePost from "./hooks/useDeletePost";
+
 
 function App() {
-  const url = 'https://jsonplaceholder.typicode.com/posts?_limit=10'
-  const titleRef = useRef<HTMLInputElement>(null)
-  const bodyRef = useRef<HTMLInputElement>(null)
-  const {mutate: deleteMutate} = useDeletePost()
-  const {mutate, isPending, error} = useCreatePost(()=>{
-    if (titleRef.current?.value && bodyRef.current?.value){
-      titleRef.current.value = "";
-      bodyRef.current.value = ""
-    }
-  })
-  const {data, isLoading} = useQuery({
-    queryKey:["posts"],
-    queryFn: () =>
-      axios.get<Post[]>(url)
-        .then((response)=>response.data)
-  })
-  return (
-    <>
-      <h2>Post</h2>
-      <form onSubmit={e =>{
-        e.preventDefault()
-        if (titleRef.current?.value && bodyRef.current?.value){
-          mutate({
-            id: 0,
-            body: bodyRef.current.value,
-            title: titleRef.current.value ,
-            userId: 1,
-          })
-        }
-      }}>
-        <div>
-          <input ref={titleRef} type="text" placeholder="titulo"/>
-        </div>
-        <div>
-          <input ref={bodyRef} type="text" placeholder="cuerpo"/>
-        </div>
-        <div>
-          <button disabled={isPending}>
-            {isPending ? 'Creando': 'Enviar'}
-          </button>
-          {error && <span>{error.message}</span>}
-        </div>
-      </form>
-      {isLoading && <p>Cargando...</p>}
-      <ul>
-        {data?.map(post => (
-          <li onClick={() => deleteMutate(post)} key={post.id}>
-            {post.title}
-            </li>
-          ))}
-      </ul>
-    </>
-  );
+  return <h2>HOLA MUNDO</h2>
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App'
 
-const queryClient = new QueryClient();
+const router = createBrowserRouter([
+  {path: "/", element: <App />},
+  {path: "/products", element: <>Hola productos</>}
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools/>
-    </QueryClientProvider>
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
