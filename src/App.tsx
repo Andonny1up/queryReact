b2c2164ca20d1d10a702be7fe20bd6202daa9644
Andoni1Barba <Andoni1barba@gmail.com>
2024-11-20 16:29:41 -0400

    react router params

--- src/App.tsx
+++
@@ -1,7 +0,0 @@
-
-
-function App() {
-  return <h2>HOLA MUNDO</h2>
-}
-
-export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
+import { RouterProvider } from 'react-router-dom'
+import router from './pages'
 
-const router = createBrowserRouter([
-  {path: "/", element: <App />},
-  {path: "/products", element: <>Hola productos</>}
-])
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
