fc273cbdc1ca6ef54d315207744408d95ca1d11f
Andoni1Barba <Andoni1barba@gmail.com>
2024-11-21 12:43:49 -0400

    aurotizacion

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,8 +1,14 @@
-import { Link } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom"
 
 type Props = {}
 
+const useUser = () => undefined;
+
 const Product = ({}: Props) => {
+  const user = useUser();
+  if (!user){
+    return <Navigate to="/"/>
+  }
   const products = [
     {id: 1, name: "iPhone"},
     {id: 2, name: "Android"}
